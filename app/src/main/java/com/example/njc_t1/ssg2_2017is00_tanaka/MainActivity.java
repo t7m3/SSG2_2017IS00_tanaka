27984d01e1020b87955742ae0095958d7c112c2c
NJC-T1 <toda7.mk3@gmail.com>
2017-12-08 11:56:41 +0900

    2017-12-08の1回目コミット

--- app/src/main/java/com/example/njc_t1/ssg2_2017is00_tanaka/MainActivity.java
+++ app/src/main/java/com/example/njc_t1/ssg2_2017is00_tanaka/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.njc_t1.ssg2_2017is00_tanaka;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.widget.ImageView;
-
-import static android.R.attr.x;
-import static android.R.attr.y;
 
 public class MainActivity extends AppCompatActivity {
 
     //グローバル変数（インスタンス変数）
     ImageViewEnemy imageViewEnemy;
+
+    //画面の幅、高さを取得する
+    DisplayMetrics dm = Resources.getSystem().getDisplayMetrics();
+    int screenWidth = dm.widthPixels;
+    int screenHeight = dm.heightPixels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         ImageView R_imageViewEnemy = (ImageView)findViewById(R.id.imageViewEnemy);
         //敵のインスタンスの生成
         int x = 0;
-        int y = 100;
+        int y = screenHeight *5/100;
         imageViewEnemy = new ImageViewEnemy(R_imageViewEnemy, x, y);
     }
 }
